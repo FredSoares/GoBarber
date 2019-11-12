597cd51fff70773bb3e0ad4234a5fe6b5ae1b804
Frederico Soares <soaresfred89@gmail.com>
2019-11-12 12:23:21 -0100

    Created user controller and the route for user registration

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,27 @@
+import User from '../models/User';
+
+class UserController {
+  // função que recebe os dados da requisição e faz a criação do utilizador na DB
+  async store(req, res) {
+    // verificar se já existe um utilizador com o mesmo email resistrado
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    // caso existir retorna erro com o status 400
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+    // registrar e pegar só os dados necessario para o retorno da resposta
+    const {
+      id, name, email, provider,
+    } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 // import da classe Router do express
 import { Router } from 'express';
 
+// import do controller do utilizador
+import UserController from './app/controllers/UserController';
 // instancia da classe Router
 const routes = new Router();
 
-// rota raiz
-routes.get('/', (req, res) => res.json({
-  message: 'Hello World!',
-}));
+// rota para criar um utilizador
+routes.post('/users', UserController.store);
 
 // export do routes
 export default routes;
