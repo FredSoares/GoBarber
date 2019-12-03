97ff88f5a2d24ac6164b9e41770ecf7e93ee3659
Frederico Soares <soaresfred89@gmail.com>
2019-12-03 14:32:35 -0100

    virtual fields in the schedule

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -24,7 +24,7 @@ class AppointmentControler {
         canceled_at: null,
       },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
